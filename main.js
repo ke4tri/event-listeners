ea9ad39706e415e7d28a56787fddcb82d2b14b2c
Wayne Collier <ke4tri@gmail.com>
2018-09-18 20:01:38 -0500

    delete button logs to console

--- main.js
+++ main.js
@@ -7,15 +7,28 @@ const toDoInputElem = document.getElementById('toDoInput');
 const notesInputElem = document.getElementById('notesInput');
 const submitToDoButtonElem = document.getElementById('submitToDoButton');
 
+const activateDeletes = () => {
+    const deleteButtons = document.getElementsByClassName('deleteButton');
+
+    for (let i = 0; i < deleteButtons.length; i++) {
+        const element = deleteButtons[i];
+        element.addEventListener("click", () => {
+            console.log("they clicked delete!!");
+        })
+    }
+    
+};
+
 const buildNewToDoCard = (toDo, notes) => {
     let domString = `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${toDo}</h5>
       <p class="card-text">${notes}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-primary deleteButton">Delete this man</a>
     </div>
   </div>`;
     printToDom(domString, 'toDoCards');
+    activateDeletes();
 }
 
 
